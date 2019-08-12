e1b5a62b054dc2e9d3aff3e5014a604823ed5289
gavin <gavin.ligthart@labela.nl>
2019-08-12 13:14:34 +0200

    Add mock data for gallery

--- src/services/appData.js
+++ src/services/appData.js
@@ -6,4 +6,50 @@ export default {
   demo_text: 'We recently released a new playable demo, and can\'t wait to share it!<br /><br />We haven\'t been sitting still at all! We added multiple new gameplay elements, levels and expanded the story line for the demo, so you can enjoy more content before we release everything.',
   about_quote: 'In the age of Enlightenment, Damien, husband and father, is returning to his homevillage of blackmill after receiving a letter from his wife. Upon arrival he discovers that everything seems abandoned. Fearing for his family he enters the village and searches for clues of the events that have occurred there.',
   about_text: 'Seek is an indie survival horror game played from a first-person perspective. A game in the style of Amnesia, Outlast and Cry of Fear, where exploring and avoiding monsters is key. The game is developed in Unreal engine 3 by an independent game development company located in Alkmaar, The Netherlands. Seek is currently in beta and changes may occur to the final product.',
+  gallery: [
+    {
+      url: 'image1',
+      type: 'image',
+    },
+    {
+      url: 'image2',
+      type: 'image',
+    },
+    {
+      url: 'image3',
+      type: 'image',
+    },
+    {
+      url: 'image4',
+      type: 'image',
+    },
+    {
+      url: 'image5',
+      type: 'image',
+    },
+    {
+      url: 'image6',
+      type: 'image',
+    },
+    {
+      url: 'image7',
+      type: 'image',
+    },
+    {
+      url: 'image8',
+      type: 'image',
+    },
+    {
+      url: 'image9',
+      type: 'image',
+    },
+    {
+      url: 'image10',
+      type: 'image',
+    },
+    {
+      url: 'video1',
+      type: 'video',
+    },
+  ],
 };
