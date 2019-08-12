63eb6431dc821e046214149160d9e62a6b8a9dda
gavin <gavin.ligthart@labela.nl>
2019-08-12 12:43:03 +0200

    Add mobile styling

--- src/components/modules/Demo/styled.js
+++ src/components/modules/Demo/styled.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import { media } from 'styles/utils';
 
 import { StyledButton } from 'common/Button/styled';
 
 export const DemoContainer = styled.div`
-  padding: 160px 0;
+  padding: 40px 0;
   background-color: ${(props) => props.theme.colors.grey};
+
+  ${media.tablet`
+    padding: 160px 0;
+  `}
 `;
 
 export const Video = styled.iframe.attrs({
@@ -12,20 +17,21 @@ export const Video = styled.iframe.attrs({
   allow: 'autoplay; encrypted-media',
   allowFullScreen: true,
 })`
-  margin-top: 50px;
-  width: 50%;
-  height: 350px;
-  position: relative;
-  z-index: 1;
+  width: 100%;
+  height: 275px;
+
+  ${media.tablet`
+    margin-top: 50px;
+    width: 50%;
+    height: 350px;
+    position: relative;
+    z-index: 1;
+  `}
 `;
 
 export const Content = styled.div`
-  position: absolute;
-  width: 55%;
-  right: 0;
-  top: 0;
   background-color: ${(props) => props.theme.colors.white};
-  padding: 50px 50px 50px 85px;
+  padding: 30px;
 
   & ${StyledButton} {
     display: inline-block;
@@ -36,6 +42,14 @@ export const Content = styled.div`
       padding-left: 36px;
     }
   }
+
+  ${media.tablet`
+    position: absolute;
+    width: 55%;
+    right: 0;
+    top: 0;
+    padding: 50px 50px 50px 85px;
+  `}
 `;
 
 export const Heading = styled.h2`
