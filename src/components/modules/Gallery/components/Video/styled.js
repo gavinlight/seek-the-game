c8cbe7c6469c035676783548082747dfd9960c9c
gavin <gavin.ligthart@labela.nl>
2019-08-13 10:38:18 +0200

    Add mobile styling to video

--- src/components/modules/Gallery/components/Video/styled.js
+++ src/components/modules/Gallery/components/Video/styled.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
+import { media } from 'styles/utils';
 
 export const StyledVideo = styled.iframe.attrs({
   frameBorder: 0,
   allow: 'autoplay; encrypted-media',
   allowFullScreen: true,
 })`
-  width: 50%;
-  height: 325px;
+  width: 95%;
+  height: 275px;
   margin: 20px auto;
+
+  ${media.tablet`
+    width: 50%;
+    height: 325px;
+  `}
 `;
