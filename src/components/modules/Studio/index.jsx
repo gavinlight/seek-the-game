ef0075b3c8970bd6e8f0880b0c78a8834d858259
gavin <gavin.ligthart@labela.nl>
2019-08-21 22:33:14 +0200

    Add base setup for studio section

---
+++ src/components/modules/Studio/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import sanitize from 'sanitize-html';
+
+import data from 'services/appData';
+import Container from 'common/Container';
+
+import { StudioContainer, Heading, Text, Logo, Url } from './styled';
+
+const Studio = () => (
+  <StudioContainer>
+    <Container variant="small">
+      <Heading>The people behind Seek</Heading>
+      <Text dangerouslySetInnerHTML={{ __html: sanitize(data.studio_content) }} />
+      <a href={data.studio_url} target="_blank" rel="noopener noreferrer">
+        <Logo src={data.studio_image} />
+      </a>
+      <Url href={data.studio_url} target="_blank" rel="noopener noreferrer">
+        {data.studio_url}
+      </Url>
+    </Container>
+  </StudioContainer>
+);
+
+export default Studio;
