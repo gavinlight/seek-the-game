9eea4f7294a2ee003f7bebae9c381a8973afe0e0
gavin <gavin.ligthart@labela.nl>
2019-08-26 11:26:45 +0200

    Add base setup for menu modal

--- src/components/common/Header/components/Menu/index.jsx
+++ src/components/common/Header/components/Menu/index.jsx
@@ -4,18 +4,47 @@ import useMediaQuery from 'hooks/useMediaQuery';
 import scrollIntoView from 'services/scrollIntoView';
 import { queries } from 'styles/utils';
 
-import { StyledMenu, MenuItem } from './styled';
+import IconClose from 'images/icon-close.png';
+
+import { StyledMenu, MenuItem, ModalBackground, ModalClose, ModalContent } from './styled';
 
 const Menu = () => {
+  const modalRef = React.useRef(null);
+  const [menuActive, setMenuActive] = React.useState(false);
+  const [activeMedia] = useMediaQuery(queries);
+
+  const setMenu = (active) => () => setMenuActive(active);
   const navigate = (to) => () => scrollIntoView(to);
 
-  return (
-    <StyledMenu>
+  const Items = () => (
+    <>
       <MenuItem onClick={navigate('#about')}>Game</MenuItem>
       <MenuItem onClick={navigate('#gallery')}>Media</MenuItem>
       <MenuItem onClick={navigate('#story')}>Prologue</MenuItem>
       <MenuItem onClick={navigate('#studio')}>Team</MenuItem>
       <MenuItem onClick={navigate('#contact')}>Contact</MenuItem>
+    </>
+  );
+
+  return (
+    <StyledMenu>
+      {activeMedia === 'mobile'
+        ? (
+          <>
+            <button type="button" onClick={setMenu(true)}>Open menu</button>
+            {menuActive && (
+              <div ref={modalRef}>
+                <ModalBackground onClick={setMenu(false)} />
+                <ModalClose onClick={setMenu(false)} src={IconClose} />
+                <ModalContent>
+                  <Items />
+                </ModalContent>
+              </div>
+            )}
+          </>
+        )
+        : <Items />
+      }
     </StyledMenu>
   );
 };
--- src/components/common/Header/components/Menu/styled.js
+++ src/components/common/Header/components/Menu/styled.js
@@ -5,6 +5,7 @@ export const StyledMenu = styled.ul`
   align-items: center;
   padding-left: 0;
   margin: 0 0 0 auto;
+  color: ${(props) => props.theme.colors.white};
 `;
 
 export const MenuItem = styled.li`
@@ -33,4 +34,31 @@ export const MenuItem = styled.li`
   &:hover:after {
     transform: scaleY(1);
   }
+`;
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 3;
+  background-color: ${(props) => props.theme.colors.black.opacity};
+  cursor: pointer;
+`;
+
+export const ModalContent = styled.div`
+  position: fixed;
+  z-index: 4;
+  left: 50%;
+  top: 25vh;
+  transform: translateX(-50%);
+`;
+
+export const ModalClose = styled.img`
+  position: fixed;
+  z-index: 4;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
 `;
