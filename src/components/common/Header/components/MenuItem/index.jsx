c801e7f10de3cf395235c736a4c8017cf8078965
gavin <gavin.ligthart@labela.nl>
2019-08-26 10:11:16 +0200

    Add styling and click handler for menu item

--- src/components/common/Header/components/MenuItem/index.jsx
+++ src/components/common/Header/components/MenuItem/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import PT from 'prop-types';
+import styled from 'styled-components';
 
-const MenuItem = ({ to, children }) => (
-  <>
-    {children}
-  </>
-);
+export const StyledMenuItem = styled.li`
+  margin-right: 20px;
+  cursor: pointer;
+  list-style: none;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+
+const MenuItem = ({ to, children }) => {
+  const navigate = () => {
+    const scrollElem = document.querySelector(to);
+
+    if (scrollElem) {
+      window.scrollTo({
+        top: scrollElem.getBoundingClientRect().top,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return (
+    <StyledMenuItem onClick={navigate}>
+      {children}
+    </StyledMenuItem>
+  );
+};
 
 MenuItem.propTypes = {
   to: PT.string,
